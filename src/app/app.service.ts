eafd0198eba135dc4c0d14bfdcfe5c08cf09efaa
krishnak03 <krishnaverma0522@gmail.com>
2024-05-29 03:37:55 +0530

    added app-service

---
+++ src/app/app.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+const BASE_URL = 'http://localhost:8080/'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient) { }
+
+  getDataFromServer(endPoint: string, requestJson?: object): Observable<any> {
+    return this.http.get(BASE_URL + endPoint, requestJson).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  postDataToServer(endPoint: string, requestJson: object): Observable<any> {
+    return this.http.post(BASE_URL + endPoint, requestJson);
+  }
+
+  putDataToServer(endPoint: string, requestJson: object): Observable<any> {
+    return this.http.put(BASE_URL + endPoint, requestJson);
+  }
+
+  deleteDataFromServer(endPoint: string, requestJson: object): Observable<any> {
+    return this.http.delete(BASE_URL + endPoint, requestJson);
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    // Log the error for debugging purposes
+    console.error('API call failed', error);
+    // Return an observable with a user-facing error message
+    return throwError(() => new Error('Something went wrong, please try again later.'));
+  }
+}
