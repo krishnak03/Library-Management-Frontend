a4ccd2d6dc13b974455f1ca2f68c043dff232861
krishnak03 <krishnaverma0522@gmail.com>
2024-05-28 08:59:44 +0530

    adding routing methods , removing html from app component and adding home component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'LMS-WEB';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.navigate(['Home']);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes, provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Home', component: HomeComponent },
+    { path: 'admin-login', component: AdminLoginComponent },
+];
+
+export const appConfig: ApplicationConfig = {
+    providers: [provideRouter(routes)]
+};
