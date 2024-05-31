0c796c7f5de7b7df1e6868709b2309545d2b6711
krishnak03 <krishnaverma0522@gmail.com>
2024-06-01 02:51:32 +0530

    adding search functionality with table.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,15 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppService } from '../app.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf } from '@angular/common';
+import { BLANK, EndPointsRefs, NavigationUrls } from '../../contants';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
+
+  searchInput = BLANK;
+  allBooksList: any;
+  bookSearchResults: any;
+  showBooksTable = false;
 
   constructor(
     private router: Router,
@@ -23,14 +31,14 @@ export class HomeComponent implements OnInit {
   }
 
   adminLogin() {
-    this.router.navigate(['admin-login']);
+    this.router.navigate([NavigationUrls.ADMIN_PAGE]);
   }
 
   getAllBooks() {
-    this.appService.getDataFromServer('books').subscribe({
+    this.appService.getDataFromServer(EndPointsRefs.BOOKS).subscribe({
       next: (response) => {
-        if (response) {
-          this.toaster.success('Successfully Loaded all books', 'Success!');
+        if (response.length > 0) {
+          this.allBooksList = response;
         } else {
           this.toaster.error('Failed to load books', 'Error!');
         }
@@ -39,6 +47,24 @@ export class HomeComponent implements OnInit {
         this.toaster.error('Something went wrong, Please try later.', 'Error!');
       }
     });
+  }
+
+  searchBooks() {
+    if (this.searchInput.length >= 3) {
+      this.bookSearchResults = this.allBooksList.filter((element: { book_name: string }) => {
+        // Check if the book_name matches the search query (case-insensitive)
+        return element.book_name.toLocaleLowerCase().includes(this.searchInput.toLocaleLowerCase());
+      });
+
+      // Check if there are any search results
+      if (this.bookSearchResults.length > 0) {
+        this.showBooksTable = true;
+      } else {
+        this.toaster.error('No books found matching the search query.', 'Error!');
+      }
+    } else {
+      this.toaster.error('Please enter more than 3 characters to search', 'Error!')
+    }
   }
 
 }
