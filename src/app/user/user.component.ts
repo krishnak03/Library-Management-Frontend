816ab27c16bdf741572a4d6036d3029bcd4bc34d
krishnak03 <krishnaverma0522@gmail.com>
2024-06-14 02:11:17 +0530

    Adding user functionality

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,94 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { BLANK, EndPointsRefs, Patterns } from '../../constants';
+import { AppService } from '../app.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+
+  getUserClicked = false;
+  addUserClicked = false;
+  updateUserClicked = false;
+  deleteUserClicked = false;
+
+
+  constructor(
+    private form_builder: FormBuilder,
+    private appService: AppService,
+    private toaster: ToastrService
+  ) { }
+
+  addUserForm = this.form_builder.group({
+    firstName: [BLANK, [Validators.required, Validators.maxLength(30), Validators.minLength(3), Validators.pattern(Patterns.NAME)]],
+    phone: [BLANK, [Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern(Patterns.PHONE)]],
+    email: [BLANK, [Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern(Patterns.EMAIL)]]
+  });
+
+  addUser() {
+    const requestJson = {
+      "user_name": this.addUserForm.controls.firstName.value,
+      "user_phone": this.addUserForm.controls.phone.value,
+      "user_email": this.addUserForm.controls.email.value
+    }
+    this.appService.postDataToServer(EndPointsRefs.USERS, requestJson).subscribe({
+      next: (response) => {
+        if (response?.success) {
+          this.toaster.success(response.message, "Success");
+        }
+      },
+      error: (error) => {
+        this.toaster.error(error.message);
+      }
+    });
+  }
+
+  toggleButton(buttonName: string) {
+    switch (buttonName) {
+      case 'addUserClicked':
+        this.addUserClicked = true;
+        break;
+      case 'getUserClicked':
+        this.getUserClicked = true;
+        break;
+      case 'updateUserClicked':
+        this.updateUserClicked = true;
+        break;
+      case 'deleteUserClicked':
+        this.deleteUserClicked = true;
+        break;
+    }
+  }
 
 }
--- src/constants.ts
+++ src/constants.ts
@@ -10,8 +10,15 @@ export const NavigationUrls = {
 
 export const EndPointsRefs = {
     BOOKS: 'books',
+    USERS: 'users',
     SEARCH: 'books/search',
     GENRES: 'genres',
     LANGUAGES: 'languages',
     ADMIN_LOGIN: 'admins/login'
+}
+
+export const Patterns = {
+    NAME: '^[a-zA-Z]+( [a-zA-Z]+)*$',
+    PHONE: '^[0-9]+$',
+    EMAIL: '^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$'
 }
